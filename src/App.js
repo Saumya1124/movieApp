e41c0d276b1f8edc0560b496aba773977696db21
Saumya1124 <24jaiswalsaumya@gmail.com>
2023-06-26 01:27:02 +0530

    new movieForm UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React , {useState} from 'react';
+import React , {useEffect, useState , useCallback} from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
 
@@ -10,15 +11,16 @@ function App() {
 
   const [isLoading , setIsLoading] = useState(false);
 
+
   
-  async function fetchMoviesHandler () {
+  const fetchMoviesHandler = useCallback( async () => {
 
     setError(null)
 
     setIsLoading(true)
 
     try{
-      const response = await fetch('https://swapi.dev/api/film/')
+      const response = await fetch('https://swapi.dev/api/films/')
 
       if(!response.ok){
         throw new Error('Something went wrong ... Retrying')
@@ -29,10 +31,6 @@ function App() {
 
       const data = await response.json()
   
-      
-
-      
-      
       
         const transformedMovies = data.results.map( movieData => {
           return {
@@ -56,13 +54,23 @@ function App() {
 
     }
    
-  }
+  } , [])
 
   const stopLoading = ()=> {
     setError(null)
   }
 
-  
+  useEffect( ()=> {
+
+    fetchMoviesHandler()
+
+  } , [fetchMoviesHandler])
+
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
 
   let content = <p>Found no movies</p>
 
@@ -80,6 +88,8 @@ function App() {
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={addMovieHandler}></AddMovie>
+        <br />
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>        
       </section>
       <section>
